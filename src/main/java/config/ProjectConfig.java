5241900c8fcff25055de14b2b767b7ba66110e0f
BM Prajwal <prajwalbm2003@gmail.com>
2025-01-21 13:08:57 +0530

    spring context: Bean Annotation v3:sameTypeMultipleBeans

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -8,9 +8,23 @@ import org.springframework.context.annotation.Configuration;
 public class ProjectConfig {
 
     @Bean
-    public Parrot parrot(){
+    public Parrot parrot1(){
         Parrot p = new Parrot();
         p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Parrot parrot2(){
+        Parrot p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean
+    public Parrot parrot3(){
+        Parrot p = new Parrot();
+        p.setName("Filo");
         return p;
     }
 
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,13 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p = context.getBean(Parrot.class);
+        Parrot p = context.getBean("parrot1", Parrot.class);
         System.out.println(p.getName());
-
-        String hello = context.getBean(String.class);
-        System.out.println(hello);
-
-        Integer n = context.getBean(Integer.class);
-        System.out.println(n);
     }
 }
