dd4045185925ff74e7d368e304095c7fd0eaf527
BM Prajwal <prajwalbm2003@gmail.com>
2025-01-21 13:05:31 +0530

    spring context: Bean Annotation v1

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import main.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot(){
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+}
